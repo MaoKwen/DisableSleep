3ae8b8e37f7312c323d7671b43ae33f3f9f7b827
MaoKwen <18361388+MaoKwen@users.noreply.github.com>
2020-05-18 21:25:34 +0800

    Update MyApplicationContext.cs

--- MyApplicationContext.cs
+++ MyApplicationContext.cs
@@ -7,6 +7,15 @@ using Microsoft.Win32;
 
 namespace DisableSleep
 {
+    [Flags]
+    enum EXECUTION_STATE : uint
+    {
+        ES_CONTINUOUS = 0x80000000,
+        ES_AWAYMODE_REQUIRED = 0x00000040,
+        ES_DISPLAY_REQUIRED = 0x00000002,
+        ES_SYSTEM_REQUIRED = 0x00000001,
+    }
+
     public class MyApplicationContext : ApplicationContext
     {
         private readonly bool useLightTheme;
@@ -15,11 +24,7 @@ namespace DisableSleep
         private readonly ContextMenu sleepContextMenu;
 
         [System.Runtime.InteropServices.DllImport("kernel32.dll")]
-        static extern uint SetThreadExecutionState(uint esFlags);
-        const uint ES_AWAYMODE_REQUIRED = 0x00000040;
-        const uint ES_CONTINUOUS = 0x80000000;
-        const uint ES_DISPLAY_REQUIRED = 0x00000002;
-        const uint ES_SYSTEM_REQUIRED = 0x00000001;
+        static extern uint SetThreadExecutionState(EXECUTION_STATE esFlags);
 
         public MyApplicationContext()
         {
@@ -42,7 +47,7 @@ namespace DisableSleep
 
         private void DisableSleep(object sender, EventArgs e)
         {
-            SetThreadExecutionState(ES_CONTINUOUS | ES_DISPLAY_REQUIRED | ES_SYSTEM_REQUIRED);
+            SetThreadExecutionState(EXECUTION_STATE.ES_CONTINUOUS | EXECUTION_STATE.ES_DISPLAY_REQUIRED | EXECUTION_STATE.ES_SYSTEM_REQUIRED);
 
             trayIcon.Icon = (useLightTheme) ? Resources.IconDisableSleepLight : Resources.IconDisableSleepDark;
             trayIcon.ContextMenu = awakeContextMenu;
@@ -50,7 +55,7 @@ namespace DisableSleep
 
         private void AllowSleep(object sender, EventArgs e)
         {
-            SetThreadExecutionState(ES_CONTINUOUS);
+            SetThreadExecutionState(EXECUTION_STATE.ES_CONTINUOUS);
 
             trayIcon.Icon = (useLightTheme) ? Resources.IconAllowSleepLight : Resources.IconAllowSleepDark;
             trayIcon.ContextMenu = sleepContextMenu;
