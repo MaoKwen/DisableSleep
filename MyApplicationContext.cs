af3634342425b3c9f498afde3188f2c2c5a17b37
MaoKwen <18361388+MaoKwen@users.noreply.github.com>
2020-05-23 01:45:18 +0800

    update to .net 4.7

--- MyApplicationContext.cs
+++ MyApplicationContext.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows.Forms;
-using DisableSleep.Properties;
+﻿using DisableSleep.Properties;
 using Microsoft.Win32;
+using System;
+using System.Windows.Forms;
 
 namespace DisableSleep
 {
